7f4c406b36b54d6121f3b33bbc26ce72258c8beb
Ankit Kumar Tiwary <ankittiwary.crp@gmail.com>
2024-07-17 21:22:29 +0530

    commit on main

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ujrlos4.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
   )
-  .then(() => app.listen(5000))
+  .then(() => app.listen(process.env.PORT || 5000))
   .catch((err) => {
     console.log(err);
   });
